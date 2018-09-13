bfb6b478c6ac4d0c930450ae6bb60d623e14fb90
root <you@example.com>
2018-09-13 19:32:02 +0800

    add privoxy

---
+++ privoxy/entrypoint.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+if [[ -z $PROXY_PORT ]]; then
+	echo "enviroment param PROXY_PORT is needed!"
+	exit 1
+fi
+sed -i "s/PROXY_PORT/${PROXY_PORT}/g" /privoxy/privoxy_cfg.conf
+
+if [[ $SOCKS_IP_PORT ]]; then
+	sed -i "s/SOCKS_PORT/${SOCKS_IP_PORT}/g" /privoxy/privoxy_cfg.conf
+else
+    echo "enviroment param PROXY_IP_PORT is needed!"
+    exit 2
+fi
+
+privoxy --no-daemon /privoxy/privoxy_cfg.conf
