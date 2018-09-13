ff5d536506b5696ebcc59433fb703b267e739ddd
root <you@example.com>
2018-09-13 20:19:16 +0800

    update privoxy/entrypoint.sh

--- privoxy/entrypoint.sh
+++ privoxy/entrypoint.sh
@@ -9,7 +9,7 @@ fi
 sed -i "s/PROXY_PORT/${PROXY_PORT}/g" /privoxy/privoxy_cfg.conf
 
 if [[ $SOCKS_IP_PORT ]]; then
-	sed -i "s/SOCKS_PORT/${SOCKS_IP_PORT}/g" /privoxy/privoxy_cfg.conf
+	sed -i "s/SOCKS_IP_PORT/${SOCKS_IP_PORT}/g" /privoxy/privoxy_cfg.conf
 else
     echo "enviroment param PROXY_IP_PORT is needed!"
     exit 2
